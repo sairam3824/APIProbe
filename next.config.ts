cd9bd9fa8ac3768bf920549cd92f0a8cab21967d
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-03-23 09:17:46 +0530

    update next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
-  },
 };
 
 export default nextConfig;
